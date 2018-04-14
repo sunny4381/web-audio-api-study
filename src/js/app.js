d359e9f672c7891a631df607f88075a2317711c3
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-04-14 13:56:42 +0900

    implement playing audio with web audio api

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,22 @@
 import 'babel-polyfill'
 
-let num = 1;
-console.log(num);
+let btn = document.querySelector("button[name=play]");
+let context = new window.AudioContext();
+
+let request = new XMLHttpRequest();
+request.open('GET', '/audio/61322__mansardian__news-end-signature.wav')
+request.responseType = 'arraybuffer';
+
+request.send();
+request.onload = function () {
+  // 読み込みが終わったら、decodeしてbufferにいれておく
+  context.decodeAudioData(request.response, function (buffer) {
+    btn.addEventListener('click', function() {
+      let source = context.createBufferSource();
+      source.buffer = buffer;
+      source.loop = true;
+      source.connect(context.destination);
+      source.start(0);
+    });
+  });
+};
