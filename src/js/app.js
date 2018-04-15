d378c387c6d6ab7f3d0c08530249a277aa0d031e
NAKANO Hideo <pinarello.marvel@gmail.com>
2018-04-15 11:07:08 +0900

    wrap around whole codes by DOMContentLoaded and protect from global name pollution

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,38 @@
 'use strict';
 import 'babel-polyfill'
 
-const btn = document.querySelector("button[name=play]");
-const context = new window.AudioContext();
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector("button[name=play]");
+  const context = new window.AudioContext();
 
-function loadAudio(url) {
-  return new Promise((fulfilled, rejected) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.responseType = 'arraybuffer';
-    request.onload = () => {
-      if (request.status === 200) {
-        fulfilled(request);
-      } else {
+  function loadAudio(url) {
+    return new Promise((fulfilled, rejected) => {
+      const request = new XMLHttpRequest();
+      request.open('GET', url);
+      request.responseType = 'arraybuffer';
+      request.onload = () => {
+        if (request.status === 200) {
+          fulfilled(request);
+        } else {
+          rejected(new Error(request));
+        }
+      };
+      request.onerror = () => {
         rejected(new Error(request));
-      }
-    };
-    request.onerror = () => {
-      rejected(new Error(request));
-    };
-    request.send();
-  });
-}
+      };
+      request.send();
+    });
+  }
 
-loadAudio('/audio/61322__mansardian__news-end-signature.wav')
-  .then((request) => { return context.decodeAudioData(request.response); })
-  .then((buffer) => {
-    btn.onclick = (ev) => {
-      const source = context.createBufferSource();
-      source.buffer = buffer;
-      source.loop = true;
-      source.connect(context.destination);
-      source.start(0);
-    };
-  });
+  loadAudio('/audio/61322__mansardian__news-end-signature.wav')
+    .then((request) => { return context.decodeAudioData(request.response); })
+    .then((buffer) => {
+      btn.onclick = (ev) => {
+        const source = context.createBufferSource();
+        source.buffer = buffer;
+        source.loop = true;
+        source.connect(context.destination);
+        source.start(0);
+      };
+    });
+});
